e8e214aa9896905c930b9bef506fe43002e07326
Diego Restrepo <restrepo@udea.edu.co>
2022-09-06 18:21:11 -0500

    fix numpy dependence with comment and proper comment

--- setup.py
+++ setup.py
@@ -63,7 +63,8 @@ def main():
 
         # Dependent packages (distributions)
         # See: https://github.com/pypa/pipenv/issues/2171
-        install_requires=[ #https://docs.scipy.org/doc/scipy/dev/toolchain.html#numpy
+        # See: https://docs.scipy.org/doc/scipy/dev/toolchain.html#numpy
+        install_requires=[
             'numpy==1.16.5; python_version=="3.7"',
             'numpy>=1.16.5; python_version=="3.8"',
             'numpy>=1.16.5; python_version=="3.9"'
