540cbac6cd754749df450be9f13377fa8123386e
Diego Restrepo <restrepo@udea.edu.co>
2022-09-06 16:00:12 -0500

    fix flake 8 and pytest

--- anomalies/anomaly.py
+++ anomalies/anomaly.py
@@ -1,58 +1,59 @@
 import numpy as np
-#!/usr/bin/env python3
-'''
+# !/usr/bin/env python3
+r'''
 Implementation of arXiv:1905.13729:
 
-A set of $N$  $n_\alpha$ integers that satisfy Diophantine 
+A set of $N$  $n_\alpha$ integers that satisfy Diophantine
 equations
-$$ 
+$$
 \sum_{\alpha=1}^{N}n_{\alpha}=0\,,\qquad  \sum_{\alpha=1}^{N}n_{\alpha}^3=0\,,
 $$
-can be parametrized as a function of two sets of integers $l$ and $k$, 
-with dimensions $(N-3)/2$ and $(N-1)/2$ for $N$ odd, 
+can be parametrized as a function of two sets of integers $l$ and $k$,
+with dimensions $(N-3)/2$ and $(N-1)/2$ for $N$ odd,
 or $N/2-1$ and $N/2-1$ for $N$ even
 '''
-def _z(l,k,sort=True,reverse=False):
-    '''
+
+
+def _z(ll, k, sort=True, reverse=False):
+    r'''
     Implementation of arXiv:1905.13729
     For l,k two set of same dimensions (or k with an extra dimension)
     return a builded array z, such that
      sum( z )=0
      sum( z**3)=0
     '''
-    l=list(l)
-    k=list(k)
-    #Build vector-like solutions x,y
-    if len(l)==len(k) :
-        x=np.array( [l[0]]+k+[-l[0]]+[-i for i in k ] )
-        y=np.array( [0,0] +l        +[-i for i in l ] )
+    ll = list(ll)
+    k = list(k)
+    # Build vector-like solutions x,y
+    if len(ll) == len(k):
+        x = np.array([ll[0]] + k + [-ll[0]] + [-i for i in k])
+        y = np.array([0, 0] + ll + [-i for i in ll])
     else:
-        x=np.array( [0]+k+[-i for i in k ] )
-        y=np.array( l+[k[0]]+[0]+[-i for i in l ]+[-k[0]])
-    xfac=0
-    yfac=0
+        x = np.array([0] + k + [-i for i in k])
+        y = np.array(ll + [k[0]] + [0] + [-i for i in ll] + [-k[0]])
     # Build not trivial solution
-    zz=(x*y**2).sum()*x-(x**2*y).sum()*y
+    zz = (x * y**2).sum() * x - (x**2 * y).sum() * y
     if sort:
-        zz=sorted( zz ,key=abs, reverse=reverse ) 
+        zz = sorted(zz, key=abs, reverse=reverse)
     return np.array(zz)
+
 
 class cfree(object):
     '''
     Add attributes to the _z function:
     * `gcd`: general common denominator
-    * `simplified`: solution with gcd=1 
+    * `simplified`: solution with gcd=1
     '''
-    def __call__(self,l,k,sort=True,reverse=False):
-        zz=_z(l,k,sort=sort,reverse=reverse)
-        self.gcd=np.gcd.reduce(zz)
-        self.simplified=(zz/self.gcd).astype(int)
+    def __call__(self, ll, k, sort=True, reverse=False):
+        zz = _z(ll, k, sort=sort, reverse=reverse)
+        self.gcd = np.gcd.reduce(zz)
+        self.simplified = (zz / self.gcd).astype(int)
         return zz
-    
-free=cfree()
-if __name__=='__main__':
-    l=input('List of integers → l=')
-    k=input('List of integers → k=')
-    sltn=free(eval(l),eval(k))
-    print( free.simplified )
 
+
+free = cfree()
+if __name__ == '__main__':
+    ll = input('List of integers → l=')
+    k = input('List of integers → k=')
+    sltn = free(eval(ll), eval(k))
+    print(free.simplified)
