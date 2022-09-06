5e6878f995147fea19b918627399c0074e16e46a
Diego Restrepo <restrepo@udea.edu.co>
2022-09-06 18:16:58 -0500

    fix numpy dependence

--- setup.py
+++ setup.py
@@ -64,7 +64,7 @@ def main():
         # Dependent packages (distributions)
         # See: https://github.com/pypa/pipenv/issues/2171
         install_requires=[
-            'numpy>=1.16.5; python_version=="3.7"',
+            'numpy==1.16.5; python_version=="3.7"',
             'numpy>=1.16.5; python_version=="3.8"',
             'numpy>=1.16.5; python_version=="3.9"'
         ],
