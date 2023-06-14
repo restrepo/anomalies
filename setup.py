a64f755f15cabc9686aaed4f71c0ac930e69b833
Diego Restrepo <restrepo@udea.edu.co>
2023-06-14 09:00:28 +0200

    Update setup.py

--- setup.py
+++ setup.py
@@ -37,7 +37,7 @@ def main():
         name="anomalies",
 
         # Version number (initial):
-        version="0.2.5",
+        version="0.2.6",
 
         # Application author details:
         author="restrepo",
